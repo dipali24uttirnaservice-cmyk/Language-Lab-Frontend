3350b67dc043ef0df5f96141212f8a955ed4b8f2
Dipali <dipali24uttirnaservice@gmail.com>
2026-07-22 11:47:22 +0530

    Authentication token is lost issue solve

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,44 +1,40 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const token = Cookies.get("token");
+    const role = Cookies.get("role");
 
-  const login = (userData, userRole) => {
-    setUser(userData);
-    setRole(userRole);
-  };
+    if (token) {
+      setRole(role);
 
+      // Call your profile API here
+      // getCurrentUser().then((res) => setUser(res.data));
+    }
 
-  const logout = () => {
-    setUser(null);
-    setRole(null);
-  };
+    setLoading(false);
+  }, []);
 
+  if (loading) return null;
 
   return (
     <AuthContext.Provider
-      value={{
-        user,
-        role,
-        login,
-        logout,
-        setUser,
-        setRole
-      }}
+      value={{ user, role, setUser, setRole }}
     >
       {children}
     </AuthContext.Provider>
   );
 }
 
-
-export function useAuth(){
+export function useAuth() {
   return useContext(AuthContext);
 }
