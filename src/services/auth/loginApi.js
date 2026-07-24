09c46cb9303fd8531bd200d245bfdb30a690a4de
ashwiniuttirnaservice <ashwiniuttirnaservice01@gmail.com>
2026-07-24 09:03:49 +0530

    update

--- src/services/auth/loginApi.js
+++ src/services/auth/loginApi.js
@@ -8,3 +8,15 @@ export const instituteLogin = async (payload) => {
   return await api.post("/institute/login", payload);
 };
 
+export const verifyInstituteCode = async (code) => {
+  return await api.get(`/institute/verify-code/${code}`);
+};
+
+export const sendInstituteOtp = async (institute_code) => {
+  return await api.post("/institute/send-otp", { institute_code });
+};
+
+export const verifyInstituteOtp = async (institute_code, otp) => {
+  return await api.post("/institute/verify-otp", { institute_code, otp });
+};
+
