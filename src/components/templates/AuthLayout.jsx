a742769bd88ce378666366d312f1bf1a517fcf5b
ashwiniuttirnaservice <ashwiniuttirnaservice01@gmail.com>
2026-08-22 15:28:25 +0530

    update Landing pange

--- src/components/templates/AuthLayout.jsx
+++ src/components/templates/AuthLayout.jsx
@@ -1,23 +1,14 @@
-export default function AuthLayout({
-  title,
-  subtitle,
-  children,
-}) {
+export default function AuthLayout({ title, subtitle, children }) {
   return (
     <section className="min-h-screen bg-slate-950">
       <div className="grid min-h-screen lg:grid-cols-2">
-
         {/* Left Side */}
 
         <div className="hidden lg:flex items-center justify-center p-10">
           <div>
-            <h1 className="text-6xl font-bold text-white">
-              LanguageLab
-            </h1>
+            <h1 className="text-6xl font-bold text-white">Uttirna DigiLabs</h1>
 
-            <p className="mt-6 text-slate-400">
-              Learn Languages With AI
-            </p>
+            <p className="mt-6 text-slate-400">Learn Languages With AI</p>
           </div>
         </div>
 
@@ -25,19 +16,13 @@ export default function AuthLayout({
 
         <div className="flex items-center justify-center p-6">
           <div className="w-full max-w-md">
+            <h2 className="text-4xl font-bold text-white">{title}</h2>
 
-            <h2 className="text-4xl font-bold text-white">
-              {title}
-            </h2>
-
-            <p className="mt-2 text-slate-400">
-              {subtitle}
-            </p>
+            <p className="mt-2 text-slate-400">{subtitle}</p>
 
             {children}
           </div>
         </div>
-
       </div>
     </section>
   );
